d8711922b9263009399b4e1e28d4db580dcfd4c3
Mateo Gannod <mcgannod42@tntech.edu>
2024-05-27 17:44:31 -0500

    Initial commit

---
+++ segment_background.py
@@ -0,0 +1,86 @@
+import cv2
+import numpy as np
+import os
+import argparse
+
+GREEN_BACKGROUND_MIN = (50, 35, 20)
+GREEN_BACKGROUND_MAX = (70, 255, 255)
+
+RED_BACKGROUND_MIN = (0, 35, 20)
+RED_BACKGROUND_MAX = (10, 255, 255)
+
+def main(args):
+    path = args.filename
+    if args.red:
+        b_min = RED_BACKGROUND_MIN
+        b_max = RED_BACKGROUND_MAX
+    else:
+        bmin = GREEN_BACKGROUND_MIN
+        bmax = GREEN_BACKGROUND_MAX
+    image_crop = args.img_crop if args.img_crop else 0
+    card_border = args.card_border if args.card_border else 10
+
+    find_cards(path, bmin, bmax, image_crop, card_border)
+
+def find_cards(img_path, background_min, background_max, image_crop = 0, card_border = 10):
+    #read in image and crop if needed
+    image_name = os.path.basename(img_path).split('.')[0]
+    img = cv2.imread(img_path)
+    img_shape = img.shape
+    img = img[image_crop:img_shape[0]-image_crop, image_crop:img_shape[1]-image_crop]
+    shape = img.shape
+
+    #convert color to hue, saturation, value (hsv) and create mask based on background color
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, background_min, background_max)
+    mask = cv2.bitwise_not(mask) #invert mask to get cards mask
+
+    smal_shape = (shape[1]//3, shape[0]//3) #resize for display
+    small_mask = cv2.resize(mask, smal_shape)
+    cv2.imshow('mask', small_mask)
+
+    #get contours of card mask
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    #copy and resizefor display
+    contour_img = img.copy()
+    cv2.drawContours(contour_img, contours, contourIdx=-1, color=(255,0,0), thickness=4, lineType=cv2.LINE_AA)
+    small_contour = cv2.resize(contour_img, smal_shape)
+    cv2.imshow('contours', small_contour)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    inp = input('Save cards? (y/n)')
+    if inp.lower() == 'y':
+        #go through contours
+        i = 0
+        for item in contours:
+            #get the area
+            rect = cv2.minAreaRect(item)
+            width, height = rect[1]
+            item_area = width * height
+            if item_area > 100000: #filter out the small ones
+                print(f'Item area {item_area}')
+                i += 1
+                s = 'found 1 card' if i < 2 else f'found {i} cards'
+                print(s)
+                #get bounding box
+                rect = cv2.minAreaRect(item)
+                box = cv2.boxPoints(rect)
+                boxpts = np.intp(box)
+                x, y, w, h = cv2.boundingRect(boxpts)
+
+                #crop out card
+                card_img = img[y-card_border:y+h+card_border, x-card_border:x+w+card_border]
+
+                #save img
+                cv2.imwrite(f'outputs/{image_name}-card{i}.png', card_img)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('Card Segmenter')
+    parser.add_argument('filename')
+    parser.add_argument('-r', '--red', action='store_true')
+    parser.add_argument('-i', '--img-crop', type=int)
+    parser.add_argument('-c', '--card-border', type=int)
+    args = parser.parse_args()
+    main(args)
---
+++ segment_cards.py
@@ -0,0 +1,71 @@
+import cv2
+import numpy as np
+
+# read the input image
+img = cv2.imread('data/bonds-thomas.png')
+
+# threshold on white
+#lower = (4, 30, 90)
+#upper = (120, 120, 120)
+lower = (190, 190, 190)
+upper = (255, 255, 255)
+#lower = (23, 44, 200)
+#upper = (73, 94, 246)
+thresh = cv2.inRange(img, lower, upper)
+
+# apply morphology
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+morph = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7,7))
+morph = cv2.morphologyEx(morph, cv2.MORPH_CLOSE, kernel)
+
+# get largest contour
+contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contours = contours[0] if len(contours) == 2 else contours[1]
+#print(len(contours[0]))
+big_contour = max(contours, key=cv2.contourArea)
+
+for item in contours:
+    if item.size > 400:
+        big_contour = item
+        # get rotated rectangle
+        rotrect = cv2.minAreaRect(big_contour)
+        (center), (width,height), angle = rotrect
+        box = cv2.boxPoints(rotrect)
+        boxpts = np.intp(box)
+
+        # draw rotated rectangle on copy of image
+        rotrect_img = img.copy()
+        cv2.drawContours(rotrect_img,[boxpts],0,(0,0,255),2)
+
+        # create mask
+        mask = np.zeros_like(img, dtype=np.uint8)
+        cv2.drawContours(mask,[boxpts],0,(255,255,255),-1)
+
+        # apply mask to input
+        result = cv2.bitwise_and(img, mask)
+
+
+        # ADDITION:
+        # get bounding box of rotated rectangle box
+        x, y, w, h = cv2.boundingRect(boxpts)
+
+        # crop image to bounding box
+        crop = img[y-5:y+h+5, x-10:x+w+5]
+
+        # save images
+        cv2.imwrite('data/baseball_card_thresh.png', thresh)
+        cv2.imwrite('data/baseball_card_morph.png', morph)
+        cv2.imwrite('data/baseball_card_rot_rect.png', rotrect_img)
+        cv2.imwrite('data/baseball_card_mask.png', mask)
+        cv2.imwrite('data/baseball_card_result.png', result)
+        cv2.imwrite('data/baseball_card_crop.png', crop)
+
+        # show results
+#        cv2.imshow('thresh', thresh)
+#        cv2.imshow('morph', morph)
+#        cv2.imshow('rotated rectangle', rotrect_img)
+        cv2.imshow('mask', mask)
+#        cv2.imshow('result', result)
+        cv2.imshow('crop', crop)
+        cv2.waitKey(0)
